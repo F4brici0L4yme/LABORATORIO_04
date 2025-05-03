12b208f263f49df1f8df8a4f8325f4c8dd5f8e4f
ChipanaGerman <gchipanaj@unsa.edu.pe>
2025-05-02 19:11:33 -0500

    Convertir archivo .md a HTML y devuelve en JSON

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
-const path = require('path');
-const express = require('express');
-const app = express();
-
-app.listen(3000, () => {
-	console.log("Escuchando en: http://localhost:3000")
-
+app.get('/api/file/:name', (req, res) => {
+    const file = req.params.name;
+    if (!file.endsWith('.md')) {
+        return res.status(400).json({error: 'Solo archivos .md permitidos'})
+    }
+    const filePath = path.join(mdDir, file);
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) return res.status(404).json({error: 'Archivo no enocntrado'});
+        const html = md.render(data);
+        res.json({html});
+    });
 });
-
-app.get('/', (request, response) => {
-	response.sendFile(path.resolve(__dirname, 'index.html'));
-});
-
