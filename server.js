cc2dd8f7f29797baf339e139d284f8e29701b80a
MauroSullcaMamani <msullcam@unsa.edu.pe>
2025-05-02 19:07:14 -0500

    Configurando nuestro servidor server.js

--- server.js
+++ server.js
@@ -1,13 +1,6 @@
+const fs = require('fs')
 const path = require('path');
 const express = require('express');
-const app = express();
-
-app.listen(3000, () => {
-	console.log("Escuchando en: http://localhost:3000")
-
-});
-
-app.get('/', (request, response) => {
-	response.sendFile(path.resolve(__dirname, 'index.html'));
-});
-
+const bp = require('body-parser')
+const MarkdownIt = require('markdown-it'),
+	md = new MarkdownIt();
