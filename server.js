77a1ddfd62f03219a97535bfb1762d353cd60c7b
MauroSullcaMamani <msullcam@unsa.edu.pe>
2025-05-02 19:14:02 -0500

    Activando las funciones del servidor

--- server.js
+++ server.js
@@ -9,3 +9,8 @@ const app = express()
 app.listen(3000, () => {
 	console.log("Escuchando en: http://localhost:3000")
 })
+
+const mdC=path.join(__dirname, 'markdowns');
+app.use(express.static(path.join(__dirname, 'pud')));
+app.use(bp.json());
+app.use(bp.urlencoded({extended: true}));
